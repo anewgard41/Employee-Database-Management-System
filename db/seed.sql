bade8cd7fa86960a354bf3e01640e97aa7b02c13
Anders Newgard <anne9707@colorado.edu>
2023-10-17 18:58:01 -0400

    fixed syntactical errors in seed.sql file. Started testing, have to switch to Linux CLI since there is an issue with inquirer and windows.

--- db/seed.sql
+++ db/seed.sql
@@ -2,27 +2,27 @@ USE employees;
 
 -- Insert sample departments
 INSERT INTO department (name) VALUES
-('Sales'),
-('Engineering'),
-('Marketing');
-('Finance');
+("Sales"),
+("Engineering"),
+("Marketing"),
+("Finance"),
 ("Biology");
 
 -- Insert sample roles
 INSERT INTO role (title, salary, department_id) VALUES
-('Sales Representative', 50000.00, 1),
-('Software Engineer', 80000.00, 2),
-('Marketing Coordinator', 45000.00, 3);
-('Accountant', 60000.00, 4);
-('Biologist', 70000.00, 5);
+("Sales Representative", 50000, 1),
+("Software Engineer", 80000, 2),
+("Marketing Coordinator", 45000, 3),
+("Accountant", 60000, 4),
+("Biologist", 70000, 5);
 
 
 
 -- Insert sample employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-('John', 'Doe', 1, NULL),
-('Jane', 'Smith', 2, 1),
-('Mike', 'Johnson', 2, 1),
-('Anna', 'Davis', 3, NULL),
-('Chris', 'Brown', 3, 4);
-('Devin', 'Homie', 5, null)
+("John", "Doe", 1, NULL),
+("Jane", "Smit", 2, 1),
+("Mike", "Johnson", 2, 1),
+("Anna", "Davis", 3, NULL),
+("Chris", "Brown", 3, 4),
+("Devin", "Homie", 5, NULL);
